5d60a2de5c53c68efc3ebc32c6940fee73362a26
jenny <jennyyin2s@gmail.com>
2022-11-09 18:01:21 -0600

    Added a skeleton carousel for themes

--- forever-and-always/src/App.js
+++ forever-and-always/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Nav from './Nav';
 import Main from './Page Bodies/Main';
+import Themes from './Page Bodies/Themes';
+import Invites from './Page Bodies/Invites';
+
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Main/>}/>
+          <Route path="/themes/" element={<Themes/>}/>
+          <Route path="/invites/" element={<Invites/>}/>
         </Routes>
       </div>
       </BrowserRouter>
