0534e448eda55481d3f69b51227d44ad472bff44
jenny <jennyyin2s@gmail.com>
2022-11-17 11:25:15 -0600

    Got the yml file to auto create databases/tables

---
+++ data/weddings.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS account (
+    account_id INT NOT NULL,
+    username varchar(12) NOT NULL,
+    user_password varchar(12) NOT NULL,
+    email varchar(30) NOT NULL,
+    budget int NOT NULL,
+    PRIMARY KEY (account_id)
+);
--- docker-entrypoint-initdb.d/create_tables.sql
+++
@@ -1,9 +0,0 @@
--- Let's create some tables! :)
-CREATE_TABLE IF NOT EXISTS account (
-    account_id INT NOT NULL,
-    username varchar(12) NOT NULL,
-    user_password varchar(12) NOT NULL,
-    email varchar(30) NOT NULL,
-    budget int NOT NULL,
-    PRIMARY KEY (account_id)
-);
