6a1948772753e5916e15d6cf453b5cbf82dda164
Michael <mchristian7897@gmail.com>
2022-12-03 19:02:18 -0500

    added functioning signup feature

--- frontend/ghi/src/Form Components/Login.js
+++ frontend/ghi/src/Form Components/Login.js
@@ -1,43 +1,43 @@
 import React from 'react'
+import SignUpModal from '../Modals/SignUpModal'
 // import {Pool, Client} from 'postgres-pool'
 
 function Login() {
-
   return (
        <div className="maincontainer">
-        <div class="container-fluid">
-            <div class="row no-gutter">
+        <div className="container-fluid">
+            <div className="row no-gutter">
 
-                <div class="col-md-6 d-none d-md-flex bg-image"></div>
+                <div className="col-md-6 d-none d-md-flex bg-image"></div>
 
-                <div class="col-md-6 bg-light">
-                    <div class="login d-flex align-items-center py-5">
+                <div className="col-md-6 bg-light">
+                    <div className="login d-flex align-items-center py-5">
 
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-lg-10 col-xl-7 mx-auto">
-                                    <h3 class="display-4">Forever and Always</h3>
-                                    <p class="text-muted mb-4">Your dream wedding awaits.</p>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-10 col-xl-7 mx-auto">
+                                    <h3 className="display-4">Forever and Always</h3>
+                                    <p className="text-muted mb-4">Your dream wedding awaits.</p>
                                     <form>
-                                        <div class="mb-3">
-                                            <input id="inputEmail" type="email" placeholder="Email address" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" />
+                                        <div className="mb-3">
+                                            <input id="inputEmail" type="email" placeholder="Email address" required="" className="form-control rounded-pill border-0 shadow-sm px-4" />
                                         </div>
-                                        <div class="mb-3">
-                                            <input id="inputPassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
-                                        </div>
-                                        <br></br>
-                                        {/* <div class="form-check">
-                                            <input id="customCheck1" type="checkbox" checked class="form-check-input" />
-                                            <label for="customCheck1" class="form-check-label">Remember password</label>
-                                        </div> */}
-                                        <div class="d-grid gap-2 mt-2">
-                                        <button type="submit" class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Login in</button>
+                                        <div className="mb-3">
+                                            <input id="inputPassword" type="password" placeholder="Password" required="" className="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
                                         </div>
 
-                                        <div class="text-center">
-                                        <p> Welcome</p>
+                                        <br></br>
+                                        {/* <div className="form-check">
+                                            <input id="customCheck1" type="checkbox" checked className="form-check-input" />
+                                            <label for="customCheck1" className="form-check-label">Remember password</label>
+                                        </div> */}
+                                        <div className="d-grid gap-2 mt-2">
+                                        <button type="submit" className="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm col-md-8 offset-md-2">Log In</button>
                                         </div>
-                                    </form>
+                                        </form>
+                                        <SignUpModal/>
+
+
                                 </div>
                             </div>
                         </div>
