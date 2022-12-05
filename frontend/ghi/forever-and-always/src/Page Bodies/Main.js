bc7c28913c901117161d84fba88c28e2927c666a
Trung Nguyen <uhnguyen2018@gmail.com>
2022-12-05 15:17:52 -0600

    main.py

--- frontend/ghi/forever-and-always/src/Page Bodies/Main.js
+++ frontend/ghi/forever-and-always/src/Page Bodies/Main.js
@@ -1,115 +1,50 @@
 import React from 'react'
 import Trung from "../images/Trung.jpeg"
-import { Card, Col, Container, Image, Row } from "react-bootstrap"
+// import { Card, Col, Container, Image, Row } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function Main() {
+  const weddingInfo = [{ "image": "https://www.theknot.com/tk-media/images/a3b7219c-ab2a-415a-a260-f6541e68af92", "title": "Build your dream wedding", "p": "Our goal is to create a platform where couples can create their dream wedding without hiring a wedding planner. By doing so, our vision was to make the planning as seamless as possible through various features on the website. So you and your forever partner can enjoy the whole process without dealing with external pressure." },
+  { "image": "https://www.millersguild.com/wp-content/uploads/2022/01/dream-of-wedding.jpg", "title": "Guidance through the unknown", "p": "Our team understands the idea to create and plan a wedding from scratch seems daunting. In order to remove the stress, we provide resources that guide the soon-to-be bride/groom into managing budget, food, venues/destinations, and other elements to make a wedding magical." },
+  { "image": "https://media2.insideweddings.com/images/YiLBjcRh1C0Uif7ZWdeW.original.jpg", "title": "Everlasting Memories with loved ones", "p": "You're one step away from living out your fairy tale wedding. We recommend creating a personal profile by signing up by pressing the button below. Afterwards, decide on what your personal budget should be and move onwards to your preferred destination. You'll be able to search and filter through different venues." }]
+
+  const teamIntro = [{ "profileImage": Trung, "cheeesyParagraph": "My goal when creating our application was to make the entire process of wedding planning fun. We wanted to help personalize every detail of the wedding while giving insight and transparent pricing.", "name": "Trung Nguyen" },
+  { "profileImage": Trung, "cheeesyParagraph": "My goal when creating our application was to make the entire process of wedding planning fun. We wanted to help personalize every detail of the wedding while giving insight and transparent pricing.", "name": "Trung Nguyen"}, 
+  { "profileImage": Trung, "cheeesyParagraph": "My goal when creating our application was to make the entire process of wedding planning fun. We wanted to help personalize every detail of the wedding while giving insight and transparent pricing.", "name": "Trung Nguyen"}, 
+  { "profileImage": Trung, "cheeesyParagraph": "My goal when creating our application was to make the entire process of wedding planning fun. We wanted to help personalize every detail of the wedding while giving insight and transparent pricing.", "name": "Trung Nguyen"}]
+
   return (
-    <div>
-      <section>
-        <div class="text-white" id="header-img">
-          <img src="https://eliaskordelakos.com/wp-content/uploads/sites/14206/2017/11/Canaves-Oia-Santorini-1.jpg" id="wedding-new_collage"  alt="..." />
+    <div className="main-pg-container">
+      <section className="header-img-container">
+        <div class="text-white" className="header-img">
+          <img src="https://www.theknot.com/tk-media/images/98ecb5ef-141a-4ef5-ba1c-bbd0e547cd28~rs_768.h" className="wedding-img" alt="..." />
         </div>
       </section>
       <main>
-        <Container>
-          <Row className="px-4 my-5">
-            <Col sm={5}>
-              <Image
-                src="https://www.theknot.com/tk-media/images/a3b7219c-ab2a-415a-a260-f6541e68af92"
-                class="img-fluid rounded-circle"
-                fluid
-                roundedCircle
-                className=""
-                />
-            </Col>
-            <Col sm={7}>
-            <h1 class="font-weigh-light">Build your dream wedding</h1>
-              <p class="mt-4">
-                Our goal is to create a platform where couples can create their dream wedding without hiring a wedding planner. By doing so, our vision was to make the planning as seamless as possible through various features on the website. So you and your forever partner can enjoy the whole process without dealing with external pressure.
-              </p>
-            </Col>
-            <Col sm={6}>
-            <h1 class="font-weigh-light">Guidance through the unknown...</h1>
-              <p class="mt-5">
-                Our team understands the idea to create and plan a wedding from scratch seems daunting. In order to remove the stress, we provide resources that guide the soon-to-be bride/groom into managing budget, food, venues/destinations, and other elements to make a wedding magical. 
-              </p>
-            </Col>
-            <Col sm={6}>
-              <Image
-                src="https://www.millersguild.com/wp-content/uploads/2022/01/dream-of-wedding.jpg"
-                class="img-fluid rounded-circle"
-                fluid
-                roundedCircle
-                className=""
-                />
-            </Col>
-            <Col sm={6}>
-              <Image
-                src="https://media2.insideweddings.com/images/YiLBjcRh1C0Uif7ZWdeW.original.jpg"
-                class="img-fluid rounded-circle"
-                fluid
-                roundedCircle
-                className=""
-                />
-            </Col>
-            <Col sm={6}>
-            <h1 class="font-weigh-light">Everlasting memories with loved ones</h1>
-              <p class="mt-5">
-                You're one step away from living out your fairy tale wedding. We recommend creating a personal profile by signing up by pressing the button below. Afterwards, decide on what your personal budget should be and move onwards to your preferred destination. You'll be able to search and filter through different venues. Next, 
-              </p>
-              <button>Getting Started</button>
-            </Col>
-          </Row>
-          <section>
-            <h1>Meet the team</h1>
-          </section>
-          <Row>
-            <Card style={{ width: '20rem' }}>
-              <Card.Img variant="top" src={Trung} id="Trung" />
-              <Card.Body>
-                <Card.Title>Trung Nguyen</Card.Title>
-                  <Card.Text>
-                    My goal when creating our application was to make the entire process of wedding planning fun. We wanted to help personalize every detail of the wedding while giving insight and transparent pricing.
-                  </Card.Text>
-              </Card.Body>
-              </Card>
-              <Card style={{ width: '20rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                  </Card.Text>
-              </Card.Body>
-              </Card>
-              <Card style={{ width: '20rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                  </Card.Text>
-              </Card.Body>
-              </Card>
-              <Card style={{ width: '20rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                  </Card.Text>
-              </Card.Body>
-              </Card>
-          </Row>
-        </Container>
+        <div className="welcome-page-container">
+          {weddingInfo.map((description, key) =>
+            <div key={key} className="purpose-container">
+              <img src={description.image} className="purpose-img"></img>
+              <div className="purpose-info-container">
+                <div className="purpose-title">{description.title}</div>
+                <div className="purpose-paragraph">{description.p}</div>
+              </div>
+            </div>
+          )}
+        </div>
+        <div className="member-cards-container">
+          {teamIntro.map((member, key) =>
+            <div className="member-card">
+              <img src={member.profileImage} className="member-img"/>
+              <div className="member-name">{member.name}</div>
+              <div className="member-vision">{member.cheeesyParagraph}</div>
+            </div>
+          )}
+        </div>
       </main>
-      <footer class="py-5 my-5 bg-dark">
-        <div class="container px-4 px-lg-5">
-          <p class="m-0 text-center text-white">
+      <footer className="main-pg-footer">
+        <div>
+          <p>
             Copyright &copy; Your Website 2021
           </p>
         </div>
