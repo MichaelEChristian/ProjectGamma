61fa4c18c6e4cd28175d1ae15ad155d14ce18304
elijah <eli.gilead@gmail.com>
2022-12-08 21:00:18 -0500

    langing pg main content completed, fixing nav

--- frontend/ghi/src/Nav.js
+++ frontend/ghi/src/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { isLoggedIn, logout } from './library/auth'
+import Flower_logo from './images/Flower_logo.png'
 
 function NavItem({ href, text }) {
   return (
@@ -27,16 +28,21 @@ function Logout() {
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+    <nav className="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0" id="nav-active">
       <div class="container">
-      <a class="navbar-brand" href="/"><span>Forever & Always</span></a>
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
+        <ul className="navbar-nav mb-lg-2">
+      <a class="navbar-brand" href="/"><span><img src={Flower_logo} height="50px"/>Forever & Always</span></a>
+
+        </ul>
+        <ul className="navbar-nav ms-auto mb-1 mb-lg-0">
           <NavItem href="/" text="Home" />
           <NavItem text="Destinations" href="/destinations" />
           <NavItem text="Themes" href="/themes" />
           <NavItem text="Invitations" href="/invites/creation" />
           <NavItem text="Menu Design" href="/menu/all/" />
+        </ul>
+        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           {!isLoggedIn() && <NavItem text="Login" href="/login" />}
           {isLoggedIn() && (
             <>
