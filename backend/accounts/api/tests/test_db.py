e1a0433825dcd3158e3e142d57dd4afe3c96e1a1
Michael <mchristian7897@gmail.com>
2022-12-12 15:29:15 -0500

    fixed linting issue

--- backend/accounts/api/tests/test_db.py
+++ backend/accounts/api/tests/test_db.py
@@ -8,17 +8,17 @@ def create_mock(dict):
 
 def test_create_record_successful():
     cursor_mock = create_mock({"description": [
-      create_mock({"name": "username"}),
-      create_mock({"name": "spouse"}),
-      create_mock({"name": "budget"}),
-      create_mock({"name": "state"})]})
+        create_mock({"name": "username"}),
+        create_mock({"name": "spouse"}),
+        create_mock({"name": "budget"}),
+        create_mock({"name": "state"})]})
     row = ("mike", "waifu", 0, "virginia")
     record = create_record(cursor_mock, row)
     compare = {
-      "username": "mike",
-      "spouse": "waifu",
-      "budget": 0,
-      "state": "virginia"}
+        "username": "mike",
+        "spouse": "waifu",
+        "budget": 0,
+        "state": "virginia"}
     assert record["username"] == compare["username"]
     assert record["spouse"] == compare["spouse"]
     assert record["budget"] == compare["budget"]
